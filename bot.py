e5d6df6e1d0ee5eae005a5ec20018dd527eb9a08
uzairname <uzair.m6d@gmail.com>
2022-03-08 01:15:07 -0500

    fix typo

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = hikari.GatewayBot(os.environ.get("DISCORD_TOKEN"))
 
 @bot.listen()
 async def ping(event: hikari.GuildMessageCreateEvent) -> None:
-    if event.is_bot() or not event.content:
+    if event.is_bot or not event.content:
         return
     if event.content.startswith("hi"):
         await event.message.respond("hello")
