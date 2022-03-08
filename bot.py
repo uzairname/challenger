df3dd5478e2383ebb18377359ee2693c72790004
uzairname <uzair.m6d@gmail.com>
2022-03-08 00:43:21 -0500

    add basic feature

---
+++ bot.py
@@ -0,0 +1,17 @@
+
+import hikari
+import os
+
+
+bot = hikari.GatewayBot(os.environ.get("DISCORD_TOKEN"))
+
+@bot.listen()
+async def ping(event: hikari.GuildMessageCreateEvent) -> None:
+    if event.is_bot() or not event.content:
+        return
+    if event.content.startswith("hi"):
+        await event.message.respond("hello")
+
+
+
+bot.run()
