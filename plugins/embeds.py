607de41409085670d71f5563b4a2b6b5c21c6fd8
uzairname <uzair.m6d@gmail.com>
2022-03-08 16:10:00 -0500

    feature: title change embed

--- plugins/embeds.py
+++ plugins/embeds.py
@@ -1,5 +1,10 @@
+import asyncio
 import hikari
 import tanjun
+
+from hikari import InteractionCreateEvent
+from hikari.interactions.base_interactions import ResponseType
+from hikari.messages import ButtonStyle
 
 from tanjun.abc import SlashContext
 
@@ -14,17 +19,54 @@ async def interactive_post(
         bot: hikari.GatewayBot = tanjun.injected(type=hikari.GatewayBot),
         client: tanjun.Client = tanjun.injected(type=tanjun.Client)
 ) -> None:
-    embed = hikari.Embed(title="New Embed")
+    client.metadata['embed'] = hikari.Embed(title="New Embed")
     row = ctx.rest.build_action_row()
     (
-        row.add_button(hikari.ButtonStyle.PRIMARY, "📋")
+        row.add_button(ButtonStyle.PRIMARY, "📋")
             .set_label("Change Title")
             .set_emoji("📋")
             .add_to_container()
     )
-    await ctx.edit_initial_response("Click/Tap your choice below, then watch the embed update!", embed=embed, components=[row, ])
+    (
+        row.add_button(ButtonStyle.DANGER, "❌")
+        .set_label("Exit")
+        .set_emoji("❌")
+        .add_to_container()
+    )
+    await ctx.edit_initial_response("Click/Tap your choice below, then watch the embed update!", embed=client.metadata['embed'], components=[row, ])
+    try:
+        async with bot.stream(InteractionCreateEvent, timeout=60).filter(('interaction.user.id', ctx.author.id)) as stream:
+            async for event in stream:
+                await event.interaction.create_initial_response(
+                    ResponseType.DEFERRED_MESSAGE_UPDATE,
+                )
+                key = event.interaction.custom_id
+                if key == "❌":
+                    await ctx.edit_initial_response(content=f"Exiting!", components=[])
+                    return
+                elif key == "📋":
+                    await title(ctx, bot, client)
+
+                await ctx.edit_initial_response("Click/Tap your choice below, then watch the embed update!", embed=client.metadata['embed'], components=[row])
+    except asyncio.TimeoutError:
+        await ctx.edit_initial_response("Waited for 60 seconds... Timeout.", embed=None, components=[])
 
 
-@ tanjun.as_loader
+
+async def title(ctx: SlashContext, bot: hikari.GatewayBot, client: tanjun.Client):
+    embed_dict, *_ = bot.entity_factory.serialize_embed(client.metadata['embed'])
+    await ctx.edit_initial_response(content="Set Title for embed:", components=[])
+    try:
+        async with bot.stream(hikari.GuildMessageCreateEvent, timeout=60).filter(('author', ctx.author)) as stream:
+            async for event in stream:
+                embed_dict['title'] = event.content[:200]
+                client.metadata['embed'] = bot.entity_factory.deserialize_embed(embed_dict)
+                await ctx.edit_initial_response(content="Title updated!", embed=client.metadata['embed'], components=[])
+                await event.message.delete()
+                return
+    except asyncio.TimeoutError:
+        await ctx.edit_initial_response("Waited for 60 seconds... Timeout.", embed=None, components=[])
+
+@tanjun.as_loader
 def load(client: tanjun.abc.Client) -> None:
     client.add_component(component.copy())
