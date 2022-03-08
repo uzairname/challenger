d2c6afcdb70a62ce1b05849a07f9fa77d7e5fa44
uzairname <uzair.m6d@gmail.com>
2022-03-08 01:06:08 -0500

    print

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
-
 import hikari
 import os
 
-
 bot = hikari.GatewayBot(os.environ.get("DISCORD_TOKEN"))
+
 
 @bot.listen()
 async def ping(event: hikari.GuildMessageCreateEvent) -> None:
@@ -12,6 +11,5 @@ async def ping(event: hikari.GuildMessageCreateEvent) -> None:
     if event.content.startswith("hi"):
         await event.message.respond("hello")
 
-
-
+print("log test!!")
 bot.run()
