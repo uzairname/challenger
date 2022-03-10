2acfb15dacf085690ec7f5d5cc655819d9443238
uzairname <uzair.m6d@gmail.com>
2022-03-10 01:21:22 -0500

    feature: read and write from postgres db

--- __main__.py
+++ __main__.py
@@ -1,29 +1,10 @@
 import os
 from bot import build_bot
 import logging
-
-import psycopg2
-
-
+from database import *
 
 
 if __name__ == "__main__":
-    try:
-        conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
-        cur = conn.cursor()
-
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        db_version = cur.fetchone()
-        print(db_version)
-
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
+    config_database()
 
     build_bot().run()
---
+++ database.py
@@ -0,0 +1,57 @@
+import psycopg2
+import os
+import functools
+
+
+def check_errors(func):
+    # for commands that take a context, respond with an error if it doesn't work
+    @functools.wraps(func)
+    def wrapper_check_errors(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("test add player: " + str(error))
+
+    return wrapper_check_errors
+
+
+@check_errors
+def config_database():
+    conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
+    cur = conn.cursor()
+
+    create_player_table(conn, cur)
+
+    print("player elo: ", test_get_elo(conn, cur, "12345"))
+    test_add_player(conn, cur, "12345678", "feap", "100")
+
+
+    #close connections
+    cur.close()
+    conn.commit()
+    if conn is not None:
+        conn.close()
+        print('Database connection closed.')
+
+@check_errors
+def test_add_player(conn, cur, new_user_id, new_username, new_elo):
+    sql = "INSERT INTO players(user_id, username, elo) VALUES(%s, %s, %s)"
+    cur.execute(sql, (new_user_id, new_username, new_elo,))
+
+@check_errors
+def test_get_elo(conn, cur, player_id):
+    sql = f"SELECT elo FROM players WHERE user_id=%s;"
+
+    cur.execute(sql, (player_id,))
+
+    return cur.fetchall()
+
+def create_player_table(conn, cur):
+    command = ("""
+        CREATE TABLE IF NOT EXISTS players (
+            user_id INT PRIMARY KEY,
+            username VARCHAR,
+            elo VARCHAR
+        )
+        """)
+    cur.execute(command)
